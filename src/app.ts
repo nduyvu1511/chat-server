2e6ad16a7d1a2ecc2e02c483313e07375707abf5
vu <duyvujarvan4@gmail.com>
2022-10-16 09:48:19 +0700

    update socket io server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import dotenv from "dotenv"
 import express from "express"
-import { createServer } from "http"
 import morgan from "morgan"
 import { Server } from "socket.io"
 import db from "./config/db"
@@ -9,7 +9,6 @@ import log from "./config/logger"
 import socketHandler from "./config/socket"
 import swaggerDocs from "./config/swagger"
 import routes from "./routes"
-import dotenv from "dotenv"
 dotenv.config()
 
 const corsConfig = {
@@ -26,22 +25,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: "10mb" }))
 app.use(cors(corsConfig))
 
-app.listen(PORT, () => {
-  log.info(`App listening at port ${PORT}`)
+// Socket io
+const httpServer = app.listen(PORT, () => {
   db.connect()
   swaggerDocs(app, Number(PORT))
   routes(app)
+  log.info(`App listening at port ${PORT}`)
+  log.info(`Socket listening at port ${PORT}`)
 })
 
-// Socket io
-const httpServer = createServer(app)
-// httpServer.listen(process.env.API_URL, () => {
-//   log.info(`Socket listening at port ${PORT}`)
-// })
-export const socket = new Server(httpServer, {
-  cors: corsConfig,
-  transports: ["websocket"],
-  path: "/",
-})
-
+export const socket = new Server(httpServer, { cors: corsConfig })
 socketHandler(socket)
